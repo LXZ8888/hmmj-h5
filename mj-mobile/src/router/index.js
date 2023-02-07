2d82dc0e5c4afb5714e3a8879ac9bb045aba926b
zhenzhen <1967668484@qq.com>
2023-02-07 15:54:52 +0800

    框架

--- mj-mobile/src/router/index.js
+++ mj-mobile/src/router/index.js
@@ -1,9 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import detail from '@/views/detail.vue'
+import layout from '@/views/layout.vue'
+import login from '@/views/login.vue'
+import register from '@/views/register.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    name: 'layout',
+    path: '/',
+    component: layout
+  },
+  { name: 'login', path: '/login', component: login },
+  {
+    name: 'register',
+    path: '/register',
+    component: register
+  },
+  {
+    name: 'detail',
+    path: '/detail',
+    component: detail
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ mj-mobile/src/views/layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="layout-page">
+    首页架子 - 内容区域
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+
+    底部tabbar - 区域
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout-page',
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
